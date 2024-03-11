333b23d7901631d88d50f5049d8f1b513071b494
Anzeela Kabir <anzeelakabir29@gmail.com>
2024-03-11 01:31:46 +0000

    Git is awesome!

---
+++ helloWorld.py
@@ -0,0 +1,12 @@
+# Practical compulsory task 1
+
+# helloWorld.py
+
+# Defining the main function.
+def main():
+	# Printing out the message.
+    print("Git is awesome!")
+
+# Executing the main function if the script is running directly.
+if __name__ == "__main__":
+    main()
